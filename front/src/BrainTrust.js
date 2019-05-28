6e56e3c1a7d52b29920498d11c55b4f5d7c9eb38
Ximena Sosa <vxsm26@gmail.com>
2019-05-28 18:26:24 -0500

    Brain trust layout Completo el diseño de brain trust, falta corregir imagenes a alfabetico

--- front/src/BrainTrust.js
+++ front/src/BrainTrust.js
@@ -1,8 +1,36 @@
 import React, { Component } from "react";
+import staffMember1 from './img/team/1.png';
+import staffMember2 from './img/team/2.png';
+import staffMember3 from './img/team/3.png';
+import staffMember4 from './img/team/4.png';
+import staffMember5 from './img/team/5.png';
+import staffMember6 from './img/team/6.png';
+import staffMember7 from './img/team/7.png';
+import staffMember8 from './img/team/8.png';
+import staffMember9 from './img/team/9.png';
+import staffMember10 from './img/team/10.png';
+import staffMember11 from './img/team/11.png';
+import staffMember12 from './img/team/12.png';
+import staffMember13 from './img/team/13.png';
+import staffMember14 from './img/team/14.png';
+import staffMember15 from './img/team/15.png';
+import staffMember16 from './img/team/16.png';
+import staffMember17 from './img/team/17.png';
+import staffMember18 from './img/team/18.png';
+import staffMember19 from './img/team/19.png';
+import staffMember20 from './img/team/20.png';
+import staffMember21 from './img/team/21.png';
+import staffMember22 from './img/team/22.png';
+import staffMember23 from './img/team/23.png';
+import staffMember24 from './img/team/24.png';
+import staffMember25 from './img/team/25.png';
+import staffMember26 from './img/team/26.png';
+import staffMember27 from './img/team/26.png';
  
 class BraingTrust extends Component {
   render() {
     return (
+      <div>
         <div className="generalContent">
           The backbone of The Tembo Group is our Brain Trust: a deep bench of extraordinary, world-changing professionals.
           <br/><br/>
@@ -10,6 +38,224 @@ class BraingTrust extends Component {
           <br/><br/>
           The Trust is a network of systems thinkers, movement-makers, and management consultants. We are design-thinkers, doers, artists and culture-shifters. We are lawyers, futurists, fundraisers, and researchers. We have been operating in the eye of the storm for many years, have seen the best brains tackle seemingly intractable problems, and know who and what it takes to get the job done.
         </div>
+        <div className="divHorLine">
+          
+        </div>
+        <div className="staffTitle">
+          KEY DIRECTORS EXPERTS
+        </div>
+        <div align="center" className="staffTable">
+          <table>
+            <tbody>
+              <tr>
+                <td>
+                  <div className="staffMember1 staffPicture" alt="staffMember"/>
+                </td>
+                <td>
+                  <div className="staffMember2 staffPicture" alt="staffMember"/>
+                </td>
+                <td>
+                  <div className="staffMember3 staffPicture" alt="staffMember"/>
+                </td>
+                <td>
+                  <div className="staffMember4 staffPicture" alt="staffMember"/>
+                </td>
+              </tr>
+              <tr className="trStaffNames">
+                <td className="staffName">
+                  - Alexa Courtney -
+                </td>
+                <td className="staffName">
+                  - Andi Cross -
+                </td>
+                <td className="staffName">
+                  - Ari Ratner -
+                </td>
+                <td className="staffName">
+                  - Christina -<br/>
+                  Lindstrom
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <div className="staffMember5 staffPicture" alt="staffMember"/>
+                </td>
+                <td>
+                  <div className="staffMember6 staffPicture" alt="staffMember"/>
+                </td>
+                <td>
+                  <div className="staffMember7 staffPicture" alt="staffMember"/>
+                </td>
+                <td>
+                  <div className="staffMember8 staffPicture" alt="staffMember"/>
+                </td>
+              </tr>
+              <tr className="trStaffNames">
+                <td className="staffName">
+                  - Denielle Sachs -
+                </td>
+                <td className="staffName">
+                  - Diana Walker -
+                </td>
+                <td className="staffName">
+                  - Diane Osgood -
+                </td>
+                <td className="staffName">
+                  - Divya Demato -
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <div className="staffMember9 staffPicture" alt="staffMember"/>
+                </td>
+                <td>
+                  <div className="staffMember10 staffPicture" alt="staffMember"/>
+                </td>
+                <td>
+                  <div className="staffMember11 staffPicture" alt="staffMember"/>
+                </td>
+                <td>
+                  <div className="staffMember12 staffPicture" alt="staffMember"/>
+                </td>
+              </tr>
+              <tr className="trStaffNames">
+                <td className="staffName">
+                  - Elizabeth -<br/>
+                  Duncan-Watt
+                </td>
+                <td className="staffName">
+                  - Eric S. Nonacs -
+                </td>
+                <td className="staffName">
+                  - Garance Choko -
+                </td>
+                <td className="staffName">
+                  - Greta Knutzen -
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <div className="staffMember13 staffPicture" alt="staffMember"/>
+                </td>
+                <td>
+                  <div className="staffMember14 staffPicture" alt="staffMember"/>
+                </td>
+                <td>
+                  <div className="staffMember15 staffPicture" alt="staffMember"/>
+                </td>
+                <td>
+                  <div className="staffMember16 staffPicture" alt="staffMember"/>
+                </td>
+              </tr>
+              <tr className="trStaffNames">
+                <td className="staffName">
+                  - Jacques-Philippe -<br/>
+                  Piverger
+                </td>
+                <td className="staffName">
+                  - Jennifer -<br/>
+                  Butte-Dahl
+                </td>
+                <td className="staffName">
+                  - Jie-Song Zhang -
+                </td>
+                <td className="staffName">
+                  - Kathryn -<br/>
+                  Harvey
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <div className="staffMember17 staffPicture" alt="staffMember"/>
+                </td>
+                <td>
+                  <div className="staffMember18 staffPicture" alt="staffMember"/>
+                </td>
+                <td>
+                  <div className="staffMember19 staffPicture" alt="staffMember"/>
+                </td>
+                <td>
+                  <div className="staffMember20 staffPicture" alt="staffMember"/>
+                </td>
+              </tr>
+              <tr className="trStaffNames">
+                <td className="staffName">
+                  - Laurie -<br/>
+                  Sztejnberg
+                </td>
+                <td className="staffName">
+                  - Marilia Bezerra -
+                </td>
+                <td className="staffName">
+                  - Michael Skolnik -
+                </td>
+                <td className="staffName">
+                  - Natalie Byrne -                  
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <div className="staffMember21 staffPicture" alt="staffMember"/>
+                </td>
+                <td>
+                  <div className="staffMember22 staffPicture" alt="staffMember"/>
+                </td>
+                <td>
+                  <div className="staffMember23 staffPicture" alt="staffMember"/>
+                </td>
+                <td>
+                  <div className="staffMember24 staffPicture" alt="staffMember"/>
+                </td>
+              </tr>
+              <tr className="trStaffNames">
+                <td className="staffName">
+                  - Pauline Zalkin -
+                </td>
+                <td className="staffName">
+                  - Rebecca Cariati -
+                </td>
+                <td className="staffName">
+                  - Sala Elise -<br/>
+                  Patterson
+                </td>
+                <td className="staffName">
+                  - Simone Pero -                  
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <div className="staffMember25 staffPicture" alt="staffMember"/>
+                </td>
+                <td>
+                  <div className="staffMember26 staffPicture" alt="staffMember"/>
+                </td>
+                <td>
+                  <div className="staffMember27 staffPicture" alt="staffMember"/>
+                </td>
+                <td>
+                  
+                </td>
+              </tr>
+              <tr className="trStaffNames">
+                <td className="staffName">
+                  - Stefeni Jung -
+                </td>
+                <td className="staffName">
+                  - Vanessa Fajans -<br/>
+                  Turner
+                </td>
+                <td className="staffName">
+                  - Yigal -<br/>
+                  Kerszenbaum
+                </td>
+                <td className="staffName">
+
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     );
   }
 }
--- front/src/Careers.js
+++ front/src/Careers.js
@@ -8,32 +8,32 @@ class Careers extends Component {
           Join our team of smart, passionate, diverse idealist<br/>
           who are on a mission to change the world.
         </div>
-        <div className="careersTable">
+        <div className="divCareersTable">
           <table cellSpacing="0">
             <tbody>
               <tr>
-                <td bgcolor="#cce6ff"><b>Engagement Manager</b></td>
+                <td className="tdCareers" bgcolor="#cce6ff"><b>Engagement Manager</b></td>
               </tr>
               <tr>
-                <td>The Engagement Manager will primary responsibility for managing daily interaction, workflow, timelines and deliverables. This is an ideal position for someone who has excellent people skills, a love of structure, and who is detail oriented but does not miss the big picture.</td>
+                <td className="tdCareers">The Engagement Manager will primary responsibility for managing daily interaction, workflow, timelines and deliverables. This is an ideal position for someone who has excellent people skills, a love of structure, and who is detail oriented but does not miss the big picture.</td>
               </tr>
               <tr bgcolor="#f2f2f2">
-                <td><br/></td>
+                <td className="tdCareers"><br/></td>
               </tr>
             </tbody>
           </table>  
         </div>
-        <div className="careersTable">
+        <div className="divCareersTable">
           <table cellSpacing="0">
             <tbody>
               <tr>
-                <td bgcolor="#cce6ff"><b>Chief of Staff</b></td>
+                <td className="tdCareers" bgcolor="#cce6ff"><b>Chief of Staff</b></td>
               </tr>
               <tr>
-                <td>The Tembo Group is seeking an Executive Assistant to support our Managing Director and perform a variety of administrative tasks across the organization. The ideal candidate is dynamic, entrepreneurial, highly organized and an excellent internet sleuth and problem solver. They are self- motivated, professional, and capable of managing their work load and prioritizing tasks in a fast-paced start-up environment.</td>
+                <td className="tdCareers">The Tembo Group is seeking an Executive Assistant to support our Managing Director and perform a variety of administrative tasks across the organization. The ideal candidate is dynamic, entrepreneurial, highly organized and an excellent internet sleuth and problem solver. They are self- motivated, professional, and capable of managing their work load and prioritizing tasks in a fast-paced start-up environment.</td>
               </tr>
               <tr bgcolor="#f2f2f2">
-                <td><br/></td>
+                <td className="tdCareers"><br/></td>
               </tr>
             </tbody>
           </table>
